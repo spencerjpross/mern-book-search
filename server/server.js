7fab2b77b00bf55b45ae62174906da5487b827dc
Spencer Ross <spencerjpross@gmail.com>
2022-11-27 18:38:41 -0700

    added schema folder and added index.js, resolvers.js and typeDefs.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
+startApolloServer(typeDefs, resolvers);
+
 app.use(routes);
 
 db.once('open', () => {
